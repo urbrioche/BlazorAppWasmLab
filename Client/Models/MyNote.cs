14384d4ac20d9af4da9151fecf3d270e86683a06
jerry <aafulin@gmail.com>
2021-12-15 21:04:47 +0800

    CH3 增加CRUD的新增與修改對話窗功能

--- Client/Models/MyNote.cs
+++ Client/Models/MyNote.cs
@@ -1,6 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorAppWasmLab.Client.Models;
 
-public class MyNote
+public class MyNote : ICloneable
 {
+    [Required(ErrorMessage = "事項不可為空白")]
     public string Title { get; set; }
+
+    public MyNote Clone()
+    {
+        return (((ICloneable)this).Clone() as MyNote)!;
+    }
+
+    object ICloneable.Clone()
+    {
+        return MemberwiseClone();
+    }
 }
