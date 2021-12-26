a5bc763c4181bd8205a67e0bf09e4c70a997b7cf
jerry <aafulin@gmail.com>
2021-12-26 20:28:15 +0800

    CH6 在 Blazor 專案內呼叫 JavaScript 程式碼 2 (使用Blazored.Modal-v6，這個版本和影片中的版本不同，程式碼也不同)

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorAppWasmLab.Client;
 using BlazorAppWasmLab.Client.Services;
+using Blazored.Modal;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IMyNoteService, MyNoteService>();
+builder.Services.AddBlazoredModal();
 
 await builder.Build().RunAsync();
