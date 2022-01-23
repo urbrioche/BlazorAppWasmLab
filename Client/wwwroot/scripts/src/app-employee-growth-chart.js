167c71775bc6f4d66de95a45cbe7ecd0556f1fbe
jerry <aafulin@gmail.com>
2022-01-23 13:40:27 +0800

    temp

---
+++ Client/wwwroot/scripts/src/app-employee-growth-chart.js
@@ -0,0 +1,6 @@
+import {EmployeeGrowthChart} from "./employee-growth-chart.js";
+debugger
+alert('a');
+const employeeGrowthChart = new EmployeeGrowthChart();
+employeeGrowthChart.render();
+export {employeeGrowthChart};
