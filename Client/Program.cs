0947120442b0b7cd87a89954761504fc75002c91
jerry <aafulin@gmail.com>
2022-02-18 20:54:32 +0800

    將OlympicWinnerViewModel的DI注入改為Transient，解決頁面切換因狀態被保留而導致異常

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,6 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddScoped<IOlympicWinnerService, OlympicWinnerService>();
-builder.Services.AddScoped<OlympicWinnerViewModel>();
+builder.Services.AddTransient<OlympicWinnerViewModel>();
 
 await builder.Build().RunAsync();
